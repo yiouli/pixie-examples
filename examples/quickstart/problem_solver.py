1f157573694a7c3329e0e8a86eae482ef59d724c
yiouli <liyiousu@gmail.com>
2026-01-15 00:32:11 -0800

    Add new example using prompt API, added corresponding prompt json file.

---
+++ examples/quickstart/problem_solver.py
@@ -0,0 +1,34 @@
+from pydantic import Field
+from pydantic_ai import Agent
+
+import pixie
+
+
+class PromptVariables(pixie.PromptVariables):
+    problem_description: str = Field(
+        ..., description="A description of the problem to be solved."
+    )
+
+
+agent_prompt = pixie.create_prompt("problem_solver", PromptVariables)
+
+
+@pixie.app
+async def example_chatbot(problem_description: str):
+    """A problem solving agent that thinks and solves complex problems.
+
+    This agent utilizes pixie's prompt libraries so you can do prompt optimization within the UI.
+
+    Args:
+        problem_description (str): A description of the problem to be solved.
+    """
+
+    agent = Agent(
+        name=agent_prompt.id,
+        instructions=await agent_prompt.compile(
+            PromptVariables(problem_description=problem_description)
+        ),
+        model="gpt-4o-mini",
+    )
+    response = await agent.run()
+    yield response.output
